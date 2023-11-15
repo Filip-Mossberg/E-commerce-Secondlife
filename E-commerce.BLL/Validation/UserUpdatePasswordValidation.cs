4b0207acaa0c171a12a1b34f9c8489e94d9d02a4
Filip Mossberg <filip.mossberg@gmail.com>
2023-11-15 08:28:52 +0100

    Updated the validation for UserUpdatePasswordRequest

--- E-commerce.BLL/Validation/UserUpdatePasswordValidation.cs
+++ E-commerce.BLL/Validation/UserUpdatePasswordValidation.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using E_commerce.Models.DTO_s.User;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace E_commerce.BLL.Validation
 {
-    internal class UserUpdatePasswordValidation
+    public class UserUpdatePasswordValidation : AbstractValidator<UserUpdatePasswordRequest>
     {
+        public UserUpdatePasswordValidation()
+        {
+            // The email is sent with the UserUpdateOasswordRequest automatically
+            RuleFor(email => email.Email).NotEmpty();
+            RuleFor(password => password.PasswordHash).NotEmpty().WithMessage("Password is requierd!")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+                .MaximumLength(128).WithMessage("Password cannot exceed 128 characters.")
+                .Matches(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).*$").WithMessage("Password needs to contain at least one uppercase letter, one digit, and one special character.");
+            RuleFor(confPassword => confPassword.ConfirmPassword)
+                .Matches("PasswordHash").WithMessage("Passwords do not match.");
+        }
     }
 }
